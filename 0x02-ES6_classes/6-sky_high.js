d66f2fcc4448ddbdb9d78073326415df4028b6d9
EslamElnajdy <eslammhamed425@gmail.com>
2024-04-26 22:28:23 +0300

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('all arguments must be a number');
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
