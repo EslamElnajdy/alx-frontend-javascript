713704e67055764a3ccaadf6ecb065dc55a56c1f
EslamElnajdy <eslammhamed425@gmail.com>
2024-05-01 02:26:16 +0300

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  students.filter((obj) => obj.location === city).map((obj) => {
+    for (const obj2 of newGrades) {
+      if (obj2.studentId === obj.id) {
+        return { ...obj, grade: obj2.grade };
+      }
+    }
+    return { ...obj, grade: 'N/A' };
+  });
+}
+
+export default updateStudentGradeByCity;
