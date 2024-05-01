4cf3de0c923c19b201769d823ef88e2a8893b39e
EslamElnajdy <eslammhamed425@gmail.com>
2024-05-01 11:45:23 +0300

    handle error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
