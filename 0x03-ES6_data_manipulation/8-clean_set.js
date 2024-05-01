f8f4a082e9007046568549c6cc9d8804e2a5ab01
EslamElnajdy <eslammhamed425@gmail.com>
2024-05-01 08:43:37 +0300

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,5 @@
 function cleanSet(set, startString) {
-
-  if (startString.length === 0) {
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
 
